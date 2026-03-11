ea1a883a8856f9eae395c5514a3c5997b86e2399
zhaoskr <49693454+zhaoskr@users.noreply.github.com>
2026-03-11 21:17:26 +0800

    Update store-data.js

--- store-data.js
+++ store-data.js
@@ -1,16 +1,16 @@
-﻿window.BUTTERIOS_PRODUCTS = [
-  { id: 1, name: "Maple Harvest Table Runner", category: "Thanksgiving", price: 24.99, badge: "Best Seller", image: "./assets/thanksgiving.svg" },
-  { id: 2, name: "Christmas Sparkle Garland", category: "Christmas", price: 32.00, badge: "New", image: "./assets/christmas.svg" },
-  { id: 3, name: "Patriotic Porch Wreath", category: "4th of July", price: 39.50, badge: "USA Pick", image: "./assets/july4.svg" },
-  { id: 4, name: "Pumpkin Lantern Set", category: "Halloween", price: 27.90, badge: "Limited", image: "./assets/halloween.svg" },
-  { id: 5, name: "Easter Bunny Centerpiece", category: "Easter", price: 29.99, badge: "Spring", image: "./assets/easter.svg" },
-  { id: 6, name: "Valentine Velvet Heart Pillows", category: "Valentine", price: 22.50, badge: "2-Pack", image: "./assets/valentine.svg" },
-  { id: 7, name: "New Year Gold Confetti Kit", category: "New Year", price: 18.99, badge: "Party", image: "./assets/newyear.svg" },
-  { id: 8, name: "Fall Leaves Door Swag", category: "Thanksgiving", price: 34.95, badge: "Handmade", image: "./assets/thanksgiving.svg" },
-  { id: 9, name: "Candy Cane Porch Lights", category: "Christmas", price: 44.00, badge: "Top Rated", image: "./assets/christmas.svg" },
-  { id: 10, name: "Haunted Mantel Banner", category: "Halloween", price: 16.75, badge: "Spooky", image: "./assets/halloween.svg" },
-  { id: 11, name: "Stars and Stripes Table Set", category: "4th of July", price: 21.99, badge: "U.S. Flag", image: "./assets/july4.svg" },
-  { id: 12, name: "Spring Blossom Wreath", category: "Easter", price: 31.20, badge: "Fresh Drop", image: "./assets/easter.svg" }
+window.BUTTERIOS_PRODUCTS = [
+  { id: 1, name: "Maple Harvest Table Runner", category: "Thanksgiving", price: 24.99, badge: "Best Seller", image: "./thanksgiving.svg" },
+  { id: 2, name: "Christmas Sparkle Garland", category: "Christmas", price: 32.00, badge: "New", image: "./christmas.svg" },
+  { id: 3, name: "Patriotic Porch Wreath", category: "4th of July", price: 39.50, badge: "USA Pick", image: "./july4.svg" },
+  { id: 4, name: "Pumpkin Lantern Set", category: "Halloween", price: 27.90, badge: "Limited", image: "./halloween.svg" },
+  { id: 5, name: "Easter Bunny Centerpiece", category: "Easter", price: 29.99, badge: "Spring", image: "./easter.svg" },
+  { id: 6, name: "Valentine Velvet Heart Pillows", category: "Valentine", price: 22.50, badge: "2-Pack", image: "./valentine.svg" },
+  { id: 7, name: "New Year Gold Confetti Kit", category: "New Year", price: 18.99, badge: "Party", image: "./newyear.svg" },
+  { id: 8, name: "Fall Leaves Door Swag", category: "Thanksgiving", price: 34.95, badge: "Handmade", image: "./thanksgiving.svg" },
+  { id: 9, name: "Candy Cane Porch Lights", category: "Christmas", price: 44.00, badge: "Top Rated", image: "./christmas.svg" },
+  { id: 10, name: "Haunted Mantel Banner", category: "Halloween", price: 16.75, badge: "Spooky", image: "./halloween.svg" },
+  { id: 11, name: "Stars and Stripes Table Set", category: "4th of July", price: 21.99, badge: "U.S. Flag", image: "./july4.svg" },
+  { id: 12, name: "Spring Blossom Wreath", category: "Easter", price: 31.20, badge: "Fresh Drop", image: "./easter.svg" }
 ];
 
 window.BUTTERIOS_CART_KEY = "butterios-cart-v1";
